187519b23890fe116e22cd96700c7a834910285a
David Jones <dgjones@gmail.com>
2019-03-15 06:38:30 -0700

    added Percy snapshots

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,6 +1,8 @@
 const should = require('chai').should()
 const puppeteer = require('puppeteer')
 const platform = require("os").platform()
+const { percySnapshot } = require('@percy/puppeteer')
+
 // We need to change the args passed to puppeteer based on the platform they're using
 const puppeteerArgs = /^win/.test(platform) ? [] : [ '--single-process' ]
 
@@ -27,6 +29,7 @@ describe('TodoMVC', function() {
 
   it('Loads the app', async function() {
     await page.goto(TEST_URL)
+    await percySnapshot(page, this.test.fullTitle())
     const mainContainer = await page.$('section.todoapp')
     should.exist(mainContainer)
   })
@@ -35,6 +38,8 @@ describe('TodoMVC', function() {
     await page.goto(TEST_URL)
     await page.type('.new-todo', 'New fancy todo')
     await page.keyboard.press('Enter')
+    await percySnapshot(page, this.test.fullTitle(), {widths: [768, 992, 1200]})
+
     const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length)
     todoCount.should.eq(1)
   })
