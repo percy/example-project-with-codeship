52e9d1d0af4e9e94ea804da2ea2b60971a60c364
David Jones <dgjones@gmail.com>
2019-03-20 10:31:28 -0700

    add Percy visual testing

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,6 +1,7 @@
 const should = require('chai').should()
 const puppeteer = require('puppeteer')
 const platform = require("os").platform()
+const { percySnapshot } = require('@percy/puppeteer')
 
 // We need to change the args passed to puppeteer based on the platform they're using
 const puppeteerArgs = /^win/.test(platform) ? [] : [ '--single-process' ]
@@ -28,12 +29,16 @@ describe('TodoMVC App', function() {
     await page.goto(TEST_URL)
     const mainContainer = await page.$('section.todoapp')
     should.exist(mainContainer)
+
+    await percySnapshot(page, this.test.fullTitle(), {widths: [768, 922, 1200]})
   })
 
   it('Accepts a new todo', async function() {
     await page.goto(TEST_URL)
     await page.type('.new-todo', 'New fancy todo')
     await page.keyboard.press('Enter')
+
+    await percySnapshot(page, this.test.fullTitle())
 
     const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length)
     todoCount.should.eq(1)
