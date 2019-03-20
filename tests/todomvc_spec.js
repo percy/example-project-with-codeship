b1d687618a11a7f5fc7e772344dbe27c62e34101
David Jones <dgjones@gmail.com>
2019-03-20 08:43:09 -0700

    demo reset

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,7 +1,6 @@
 const should = require('chai').should()
 const puppeteer = require('puppeteer')
 const platform = require("os").platform()
-const { percySnapshot } = require('@percy/puppeteer')
 
 // We need to change the args passed to puppeteer based on the platform they're using
 const puppeteerArgs = /^win/.test(platform) ? [] : [ '--single-process' ]
@@ -9,11 +8,10 @@ const puppeteerArgs = /^win/.test(platform) ? [] : [ '--single-process' ]
 const TEST_URL = "http://localhost:8000"
 
 describe('TodoMVC', function() {
-
   let browser = null
   let page = null
+
   beforeEach(async function() {
-    // Create a new Puppeteer browser instace for each test case
     browser = await puppeteer.launch({
       headless: true,
       timeout: 10000,
@@ -23,13 +21,11 @@ describe('TodoMVC', function() {
   })
 
   afterEach(function() {
-    // Close the Puppeteer browser instance.
     browser.close()
   })
 
   it('Loads the app', async function() {
     await page.goto(TEST_URL)
-    await percySnapshot(page, this.test.fullTitle())
     const mainContainer = await page.$('section.todoapp')
     should.exist(mainContainer)
   })
@@ -38,7 +34,6 @@ describe('TodoMVC', function() {
     await page.goto(TEST_URL)
     await page.type('.new-todo', 'New fancy todo')
     await page.keyboard.press('Enter')
-    await percySnapshot(page, this.test.fullTitle(), {widths: [768, 992, 1200]})
 
     const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length)
     todoCount.should.eq(1)
